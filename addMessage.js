5e47a181f1632655ffba987c9b5d2009514ccabc
Hamza <hamzagaziuddin04@gmail.com>
2018-04-24 21:03:39 -0500

    Update addMessage.js

--- addMessage.js
+++ addMessage.js
@@ -2,11 +2,12 @@ javascript:
 var q = document.createElement('script');
 q.src = 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.0/socket.io.js';
 document.body.appendChild(q);
+console.log("waow it worked PT1");
 // ENTER, THEN PASTE THIS IN 
 javascript:
 var socket = io.connect('https://evening-wave-36315.herokuapp.com/');
 socket.emit('apiMsg', prompt('What is your nickname?') || "this user failed to put a name", prompt('What is your message') || "this user failed to put a message");
 socket.on('good', function(){
     alert("Message was sent");
-    console.log("waow it worked");
+    console.log("waow it worked PT2");
 });
