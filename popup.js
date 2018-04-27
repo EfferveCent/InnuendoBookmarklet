a1219e828c6b3d1b65e9b2e4ac6e626870582a53
Hamza <hamzagaziuddin04@gmail.com>
2018-04-26 21:11:56 -0500

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,3 @@
+newwindow=window.open("https://evening-wave-36315.herokuapp.com/","waow",'height=300,width=500');
+       if (window.focus) {newwindow.focus()}
+//COPY AND PASTE INTO BOOKMARKLET TO CREATE A POPUP
